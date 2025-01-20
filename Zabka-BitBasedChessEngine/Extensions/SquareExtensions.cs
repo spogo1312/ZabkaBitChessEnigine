738287c32ff4649b4da6c1c8eff8481dd086e875
spogo1312 <91920071+spogo1312@users.noreply.github.com>
2025-01-20 21:48:32 +0100

    Move class

---
+++ Zabka-BitBasedChessEngine/Extensions/SquareExtensions.cs
@@ -0,0 +1,11 @@
+﻿public static class SquareExtensions
+{
+    // Converts a square to standard chess notation (e.g., E2)
+    public static string ToChessNotation(this Square square)
+    {
+        int sq = (int)square;
+        char file = (char)('a' + (sq % 8));
+        int rank = (sq / 8) + 1;
+        return $"{file}{rank}";
+    }
+}
---
+++ Zabka-BitBasedChessEngine/Structs/Move.cs
@@ -0,0 +1,29 @@
+﻿public struct Move
+{
+    public Square From { get; set; }
+    public Square To { get; set; }
+    public PieceType Promotion { get; set; } = PieceType.None;
+    public bool IsCapture { get; set; } = false;
+    public bool IsEnPassant { get; set; } = false;
+    public bool IsCastling { get; set; } = false;
+
+    // Explicit constructor
+    public Move(Square from, Square to, PieceType promotion = PieceType.None, bool isCapture = false, bool isEnPassant = false, bool isCastling = false)
+    {
+        From = from;
+        To = to;
+        Promotion = promotion;
+        IsCapture = isCapture;
+        IsEnPassant = isEnPassant;
+        IsCastling = isCastling;
+    }
+    public override string ToString()
+    {
+        string moveStr = $"{From.ToChessNotation()}-{To.ToChessNotation()}";
+        if (Promotion != PieceType.None)
+        {
+            moveStr += $"={Promotion.ToString()[0]}";
+        }
+        return moveStr;
+    }
+}
