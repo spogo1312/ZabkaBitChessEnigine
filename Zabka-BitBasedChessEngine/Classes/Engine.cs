353d815f4cd62af13e09beb1290faf1cbcafd7b9
spogo1312 <91920071+spogo1312@users.noreply.github.com>
2025-01-20 22:11:50 +0100

    Simple Engine Logic and UCI

---
+++ Zabka-BitBasedChessEngine/Classes/Engine.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Engine
+{
+    private MoveGenerator moveGenerator;
+
+    public Engine()
+    {
+        moveGenerator = new MoveGenerator();
+    }
+
+    // Make a move on the board
+    public void MakeMove(Board board, Move move)
+    {
+        // Remove piece from the 'from' square
+        RemovePiece(board, move.From);
+
+        // Handle captures
+        if (move.IsCapture)
+        {
+            RemovePiece(board, move.To);
+        }
+
+        // Handle promotions
+        if (move.Promotion != PieceType.None)
+        {
+            AddPiece(board, move.To, move.Promotion, board.SideToMove);
+        }
+        else
+        {
+            // Move the piece to the 'to' square
+            AddPiece(board, move.To, GetPieceType(board, move.From), board.SideToMove);
+        }
+
+        // Update pawn bitboards if necessary (e.g., en passant)
+        // TODO: Implement en passant and castling
+
+        // Switch side to move
+        board.SideToMove = board.SideToMove == Color.White ? Color.Black : Color.White;
+    }
+
+    // Find the best move using a simple evaluation (e.g., random move)
+    public Move FindBestMove(Board board, Color color)
+    {
+        List<Move> moves = moveGenerator.GenerateAllMoves(board, color);
+        if (moves.Count == 0)
+            throw new InvalidOperationException("No legal moves available");
+
+        // Simple evaluation: choose a random move for demonstration
+        Random rand = new Random();
+        return moves[rand.Next(moves.Count)];
+    }
+
+    // Evaluate the board position (simple material count)
+    public int Evaluate(Board board)
+    {
+        int score = 0;
+
+        // Material weights
+        Dictionary<PieceType, int> pieceValues = new Dictionary<PieceType, int>
+        {
+            { PieceType.Pawn, 100 },
+            { PieceType.Knight, 320 },
+            { PieceType.Bishop, 330 },
+            { PieceType.Rook, 500 },
+            { PieceType.Queen, 900 },
+            { PieceType.King, 20000 }
+        };
+
+        // White material
+        score += CountPieces(board.WhitePawns, pieceValues[PieceType.Pawn]);
+        score += CountPieces(board.WhiteKnights, pieceValues[PieceType.Knight]);
+        score += CountPieces(board.WhiteBishops, pieceValues[PieceType.Bishop]);
+        score += CountPieces(board.WhiteRooks, pieceValues[PieceType.Rook]);
+        score += CountPieces(board.WhiteQueens, pieceValues[PieceType.Queen]);
+        score += CountPieces(board.WhiteKing, pieceValues[PieceType.King]);
+
+        // Black material
+        score -= CountPieces(board.BlackPawns, pieceValues[PieceType.Pawn]);
+        score -= CountPieces(board.BlackKnights, pieceValues[PieceType.Knight]);
+        score -= CountPieces(board.BlackBishops, pieceValues[PieceType.Bishop]);
+        score -= CountPieces(board.BlackRooks, pieceValues[PieceType.Rook]);
+        score -= CountPieces(board.BlackQueens, pieceValues[PieceType.Queen]);
+        score -= CountPieces(board.BlackKing, pieceValues[PieceType.King]);
+
+        return score;
+    }
+
+    private int CountPieces(Bitboard bitboard, int value)
+    {
+        return bitboard.Count() * value;
+    }
+
+    private PieceType GetPieceType(Board board, Square square)
+    {
+        if (board.WhitePawns.IsSet(square)) return PieceType.Pawn;
+        if (board.WhiteKnights.IsSet(square)) return PieceType.Knight;
+        if (board.WhiteBishops.IsSet(square)) return PieceType.Bishop;
+        if (board.WhiteRooks.IsSet(square)) return PieceType.Rook;
+        if (board.WhiteQueens.IsSet(square)) return PieceType.Queen;
+        if (board.WhiteKing.IsSet(square)) return PieceType.King;
+
+        if (board.BlackPawns.IsSet(square)) return PieceType.Pawn;
+        if (board.BlackKnights.IsSet(square)) return PieceType.Knight;
+        if (board.BlackBishops.IsSet(square)) return PieceType.Bishop;
+        if (board.BlackRooks.IsSet(square)) return PieceType.Rook;
+        if (board.BlackQueens.IsSet(square)) return PieceType.Queen;
+        if (board.BlackKing.IsSet(square)) return PieceType.King;
+
+        return PieceType.None;
+    }
+
+    private void AddPiece(Board board, Square square, PieceType pieceType, Color color)
+    {
+        switch (pieceType)
+        {
+            case PieceType.Pawn:
+                if (color == Color.White) board.WhitePawns.Set(square);
+                else board.BlackPawns.Set(square);
+                break;
+            case PieceType.Knight:
+                if (color == Color.White) board.WhiteKnights.Set(square);
+                else board.BlackKnights.Set(square);
+                break;
+            case PieceType.Bishop:
+                if (color == Color.White) board.WhiteBishops.Set(square);
+                else board.BlackBishops.Set(square);
+                break;
+            case PieceType.Rook:
+                if (color == Color.White) board.WhiteRooks.Set(square);
+                else board.BlackRooks.Set(square);
+                break;
+            case PieceType.Queen:
+                if (color == Color.White) board.WhiteQueens.Set(square);
+                else board.BlackQueens.Set(square);
+                break;
+            case PieceType.King:
+                if (color == Color.White) board.WhiteKing.Set(square);
+                else board.BlackKing.Set(square);
+                break;
+        }
+    }
+
+    private void RemovePiece(Board board, Square square)
+    {
+        if (board.WhitePawns.IsSet(square)) board.WhitePawns.Clear(square);
+        if (board.WhiteKnights.IsSet(square)) board.WhiteKnights.Clear(square);
+        if (board.WhiteBishops.IsSet(square)) board.WhiteBishops.Clear(square);
+        if (board.WhiteRooks.IsSet(square)) board.WhiteRooks.Clear(square);
+        if (board.WhiteQueens.IsSet(square)) board.WhiteQueens.Clear(square);
+        if (board.WhiteKing.IsSet(square)) board.WhiteKing.Clear(square);
+
+        if (board.BlackPawns.IsSet(square)) board.BlackPawns.Clear(square);
+        if (board.BlackKnights.IsSet(square)) board.BlackKnights.Clear(square);
+        if (board.BlackBishops.IsSet(square)) board.BlackBishops.Clear(square);
+        if (board.BlackRooks.IsSet(square)) board.BlackRooks.Clear(square);
+        if (board.BlackQueens.IsSet(square)) board.BlackQueens.Clear(square);
+        if (board.BlackKing.IsSet(square)) board.BlackKing.Clear(square);
+    }
+}
---
+++ Zabka-BitBasedChessEngine/Classes/UCI.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class UCIHandler
+{
+    private Board board;
+    private MoveGenerator moveGenerator;
+    private Engine engine;
+
+    public UCIHandler(Board board, MoveGenerator moveGenerator, Engine engine)
+    {
+        this.board = board;
+        this.moveGenerator = moveGenerator;
+        this.engine = engine;
+    }
+
+    public void HandleInput()
+    {
+        string input;
+        while ((input = Console.ReadLine()) != null)
+        {
+            if (input == "uci")
+            {
+                Console.WriteLine("id name BitboardChessEngine");
+                Console.WriteLine("id author YourName");
+                Console.WriteLine("uciok");
+            }
+            else if (input.StartsWith("isready"))
+            {
+                Console.WriteLine("readyok");
+            }
+            else if (input.StartsWith("ucinewgame"))
+            {
+                board.Initialize();
+            }
+            else if (input.StartsWith("position"))
+            {
+                ParsePosition(input);
+            }
+            else if (input.StartsWith("go"))
+            {
+                ParseGo(input);
+            }
+            else if (input.StartsWith("quit"))
+            {
+                break;
+            }
+            // Handle other UCI commands as needed
+        }
+    }
+
+    private void ParsePosition(string input)
+    {
+        // Example: position startpos moves e2e4 e7e5
+        if (input.Contains("startpos"))
+        {
+            board.Initialize();
+            int movesStart = input.IndexOf("moves");
+            if (movesStart != -1)
+            {
+                string movesStr = input.Substring(movesStart + 6);
+                string[] moves = movesStr.Split(' ');
+                foreach (var moveStr in moves)
+                {
+                    Move move = ParseMove(moveStr);
+                    engine.MakeMove(board, move);
+                }
+            }
+        }
+        else if (input.Contains("fen"))
+        {
+            // Implement FEN parsing if needed
+            // TODO: Add FEN parsing to set up arbitrary positions
+        }
+    }
+
+    private void ParseGo(string input)
+    {
+        // Implement search parameters (depth, time, etc.)
+        // For simplicity, we'll start a basic search
+        Move bestMove = engine.FindBestMove(board, board.SideToMove);
+        Console.WriteLine($"bestmove {bestMove}");
+    }
+
+    private Move ParseMove(string moveStr)
+    {
+        // Parse a move in UCI format (e.g., e2e4, e7e8q)
+        if (moveStr.Length < 4)
+            throw new ArgumentException("Invalid move string");
+
+        Square from = ParseSquare(moveStr.Substring(0, 2));
+        Square to = ParseSquare(moveStr.Substring(2, 2));
+        PieceType promotion = PieceType.None;
+
+        if (moveStr.Length > 4)
+        {
+            char promoChar = moveStr[4];
+            promotion = promoChar switch
+            {
+                'q' => PieceType.Queen,
+                'r' => PieceType.Rook,
+                'b' => PieceType.Bishop,
+                'n' => PieceType.Knight,
+                _ => PieceType.None
+            };
+        }
+
+        // Determine if it's a capture (optional, for more accurate move representation)
+        bool isCapture = false;
+        // You can enhance this by checking if the 'to' square is occupied by an enemy piece
+
+        return new Move
+        {
+            From = from,
+            To = to,
+            Promotion = promotion,
+            IsCapture = isCapture
+        };
+    }
+
+    private Square ParseSquare(string squareStr)
+    {
+        if (squareStr.Length != 2)
+            throw new ArgumentException("Invalid square string");
+
+        char file = squareStr[0];
+        char rank = squareStr[1];
+
+        int fileNum = file - 'a';
+        int rankNum = rank - '1';
+
+        if (fileNum < 0 || fileNum > 7 || rankNum < 0 || rankNum > 7)
+            throw new ArgumentException("Invalid square string");
+
+        return (Square)(rankNum * 8 + fileNum);
+    }
+}
